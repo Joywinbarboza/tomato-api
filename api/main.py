af1f8ab1412726d529df54bef2160ee2d7aa98c6
Joywinbarboza <joywinbarboza420@gmail.com>
2024-10-23 22:34:26 +0530

    1.0

---
+++ api/main.py
@@ -0,0 +1,62 @@
+from fastapi import FastAPI,File,UploadFile
+import uvicorn
+import numpy as np
+from io import BytesIO
+from PIL import Image
+import tensorflow as tf
+ 
+
+app = FastAPI()
+
+MODEL = tf.keras.models.load_model(
+    "/home/joywin-barboza/Desktop/tomato_api/model/kaggle6.keras",
+)
+
+
+CLASS_NAMES = [
+    "Tomato___Bacterial_spot",
+    "Tomato___Early_blight",
+    "Tomato___Late_blight",
+    "Tomato___Leaf_Mold",
+    "Tomato___Septoria_leaf_spot",
+    "Tomato___Spider_mites Two-spotted_spider_mite",
+    "Tomato___Target_Spot",
+    "Tomato___Tomato_Yellow_Leaf_Curl_Virus",
+    "Tomato___Tomato_mosaic_virus",
+    "Tomato___healthy"
+]
+
+
+@app.get("/")
+async def ping():
+    return "Hello, I am alive"
+
+def read_file_as_image(data) -> np.ndarray:
+    image = np.array(Image.open(BytesIO(data)))
+    
+    
+    return image
+
+    
+    return 
+    
+@app.post("/predict")
+async def predict(
+    file: UploadFile = File(...)
+):
+    image = read_file_as_image(await file.read())
+    
+    img_batch = np.expand_dims(image,0)
+    predictions = MODEL.predict(img_batch)
+    predicted_class = CLASS_NAMES[np.argmax(predictions[0])]
+    confidence = np.max(predictions[0])
+    
+    print(predictions)
+    return {
+        'class' : predicted_class,
+        'confidence' : float(confidence)
+    }
+    
+
+if __name__ == "__main__":
+    uvicorn.run(app,host='0.0.0.0',port=8000)
